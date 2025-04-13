2132cf78720cbf9e50baffba84e7d973d14a7e30
tseegiii12 <155141901+tseegiii12@users.noreply.github.com>
2025-04-13 14:58:35 +0800

    nothing changes

--- app/Http/Controllers/Grading/GradingComponentController.php
+++ app/Http/Controllers/Grading/GradingComponentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Grading;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\GradingComponent;
+
 class GradingComponentController extends Controller
 {
     public function index()
