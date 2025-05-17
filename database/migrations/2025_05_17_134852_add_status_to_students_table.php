1f330c1644744181610a407af3a8e03f3d460884
tseegiii12 <155141901+tseegiii12@users.noreply.github.com>
2025-05-17 14:42:10 +0800

    updated student migration ->  added student status

---
+++ database/migrations/2025_05_17_134852_add_status_to_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+            Schema::table('students', function (Blueprint $table) {
+                $table->enum('status', ['active','dropped', 'graduated', 'suspended'])
+                      ->default('active')
+                      ->after('proposed_number');
+            });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
